cdac977fb6b7327ae0e411fdce35245abae57a01
gujiu21L <2897795436@qq.com>
2024-02-03 18:34:44 +0800

    新建浮动按钮(FAB) & 验证点击 对应《轻松上手：＜Android Studio笔记应用开发＞（一）入门与笔记应用浅开发》的第三部分的(一)(二) 博客地址： CSDN：https://blog.csdn.net/qq_73342379/article/details/132956363 稀土掘金：https://juejin.cn/post/7284143489737719848#heading-17

--- app/src/main/java/com/example/my_notes_record/MainActivity.java
+++ app/src/main/java/com/example/my_notes_record/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.my_notes_record;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    final String TAG ="孤酒21L";
+    FloatingActionButton btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn=(FloatingActionButton) findViewById(R.id.floatingActionButton4);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG,"OnClick:Click");
+            }
+        });
     }
+
 }
